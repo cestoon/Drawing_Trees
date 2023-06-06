a95e2d2cd8ccc7ef0586a898936c5e4b7acd5e44
alina <alina.kaly@gmail.com>
2023-06-06 13:40:11 +0200

    Mads/Alina draw trees in plotly

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,3 +1,35 @@
-﻿
+﻿open Library.TreeDesign
+open Lines
 
 open Plotly.NET
+open Plotly.NET.LayoutObjects
+
+
+let t1 = Node("A", [])
+let t2 = Node("B", [])
+
+let t3 = Node("C", [ t1; t2 ])
+let t4 = Node("D", [])
+
+let t = Node("Root", [ t3; t4 ])
+
+
+let dTree = design t
+let absTree = absolutePositionTree dTree
+
+
+let points = nodeToPoint absTree
+let lines = treeLines absTree
+
+let mirroredXAxis =
+    LinearAxis.init (ShowLine = false, Mirror = StyleParam.Mirror.False, ShowGrid = false)
+
+let mirroredYAxis =
+    LinearAxis.init (ShowLine = false, Mirror = StyleParam.Mirror.False, ShowGrid = false)
+
+
+points @ lines
+|> Chart.combine
+|> Chart.withXAxis mirroredXAxis
+|> Chart.withYAxis mirroredYAxis
+|> Chart.show
--- src/App/TreeLines.fs
+++ src/App/TreeLines.fs
@@ -1,10 +1,49 @@
-module Lines 
-    open Library.Jensen.TreeFit
+module Lines
 
-    let fromTree t =
-        let rec fromTree' depth pos acc (Node((_, c), ts)) =
-            let acc' = (c + pos, -depth)::acc
-            List.fold (fromTree' (depth + 1) (pos + c)) acc' ts
-        fromTree' 0 0.0 [] t
+open Library.TreeDesign
+open Plotly.NET
 
 
+
+let nodePositions t =
+    let rec fromTree' depth pos acc (Node((_, c), ts)) =
+        let acc' = (c + pos, -depth) :: acc
+        List.fold (fromTree' (depth + 1) (pos + c)) acc' ts
+
+    fromTree' 0 0.0 [] t
+
+
+let absolutePositionTree t =
+    let rec absolutePositionTree' depth pos (Node((l, p), ts)) =
+        let absPos = p + pos
+        let childDepth = depth + 1.
+        Node((l, (absPos, -depth)), List.map (absolutePositionTree' childDepth absPos) ts)
+
+    absolutePositionTree' 0 0. t
+
+
+let nodeToPoint t =
+    let rec nodeToPoint' acc (Node((l, p), ts)) =
+        let point =
+            Chart.Point(
+                [ p ],
+                MultiText = [ l ],
+                MultiTextPosition = [ StyleParam.TextPosition.TopCenter ],
+                ShowLegend = false
+            )
+
+        point :: List.fold nodeToPoint' acc ts
+
+    nodeToPoint' [] t
+
+
+let treeLines t =
+    let rec treeLines' acc ((Node((_, p), ts)) as t) =
+        let makeLine (Node((_, p), _)) (Node((_, c), _)) =
+            Chart.Line(xy = [ p; c ], LineColor = Color.fromString "black", ShowLegend = false)
+
+        let linesToChildren = ts |> List.map (makeLine t)
+        let childLines = List.fold treeLines' acc ts
+        linesToChildren @ childLines //? TODO: Use function accumulator if optimization is needed
+
+    treeLines' [] t
