f2d6a5a695345214618bcbed931ff3e627eb84d8
Goju Ryu <mads1606@gmail.com>
2023-06-06 16:34:27 +0200

    Add main function and fix for snap apps

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,9 +1,10 @@
-﻿open Library.TreeDesign
-open Lines
+﻿open System.IO
+open Library.TreeDesign
 
 open Plotly.NET
 open Plotly.NET.LayoutObjects
 
+open Lines
 
 let t1 = Node("A", [])
 let t2 = Node("B", [])
@@ -21,15 +22,52 @@ let absTree = absolutePositionTree dTree
 let points = nodeToPoint absTree
 let lines = treeLines absTree
 
-let mirroredXAxis =
+let mirroredXAxis' =
     LinearAxis.init (ShowLine = false, Mirror = StyleParam.Mirror.False, ShowGrid = false)
 
-let mirroredYAxis =
+let mirroredYAxis' =
     LinearAxis.init (ShowLine = false, Mirror = StyleParam.Mirror.False, ShowGrid = false)
 
 
 points @ lines
 |> Chart.combine
-|> Chart.withXAxis mirroredXAxis
-|> Chart.withYAxis mirroredYAxis
+|> Chart.withXAxis mirroredXAxis'
+|> Chart.withYAxis mirroredYAxis'
 |> Chart.show
+
+//################################################
+//#     Code for main function starts below      #
+//################################################
+let mirroredXAxis =
+    LinearAxis.init (ShowLine = false, Mirror = StyleParam.Mirror.False, ShowGrid = false, Visible = false)
+
+let mirroredYAxis =
+    LinearAxis.init (ShowLine = false, Mirror = StyleParam.Mirror.False, ShowGrid = false, Visible = false)
+
+let getSavePath() =
+    let folder = "./out"
+    let path = $"{folder}/Tree-{System.DateTime.Now.ToFileTime()}.html"
+    
+    if not (Directory.Exists folder) then Directory.CreateDirectory folder |> ignore
+    path
+
+let parseTree = function
+    | Some s -> t //TODO: Insert actual implementation here if necessary
+    | None -> ExampleTrees.getRandomTree() 
+
+[<EntryPoint>]
+let main argv = 
+    argv
+    |> Array.tryHead
+    |> parseTree
+    |> design
+    |> absolutePositionTree
+    |> (fun tree -> nodeToPoint tree @ treeLines tree)
+    |> Chart.combine
+    |> Chart.withXAxis mirroredXAxis
+    |> Chart.withYAxis mirroredYAxis
+    |> Chart.saveHtml (
+        path = getSavePath(), 
+        OpenInBrowser = true
+    )
+    0 // Return successful status code
