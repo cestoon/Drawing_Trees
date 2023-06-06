c197d25bd728c955d8124e083d00a3153a1188ea
fengbingkun001 <fengbingkun001@ke.com>
2023-06-06 11:58:37 +0200

    import fsCheck

--- src/Tests/UnitTest1.fs
+++ src/Tests/UnitTest1.fs
@@ -1,6 +1,9 @@
 module Tests
 
 open NUnit.Framework
+open FsCheck
+open FsCheck.NUnit
+open Library.TreeDesign
 
 [<SetUp>]
 let Setup () =
@@ -8,4 +11,22 @@ let Setup () =
 
 [<Test>]
 let Test1 () =
+    printfn "123"
     Assert.Pass()
+
+
+type TreeDesignTests() = 
+
+    [<Property>]
+    let ``movetree should not change the tree label`` (label: string, x: float, y: float, subtrees: list<Tree<'a>>) =
+        let tree = Node((label, x), subtrees)
+        let movedTree = movetree (tree, y)
+        match movedTree with
+        | Node((movedLabel, _), _) -> movedLabel = label
+
+    [<Property>]
+    let ``movetree should add y to the tree x position`` (label: string, x: float, y: float, subtrees: list<Tree<'a>>) =
+        let tree = Node((label, x), subtrees)
+        let movedTree = movetree (tree, y)
+        match movedTree with
+        | Node((_, movedX), _) -> movedX = x + y
