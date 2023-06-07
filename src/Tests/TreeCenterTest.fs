2e8c43d8b97a1c7cf76eadcf2778e59857328a7d
fengbingkun001 <fengbingkun001@ke.com>
2023-06-07 14:40:37 +0200

    center test

--- src/Tests/TreeCenterTest.fs
+++ src/Tests/TreeCenterTest.fs
@@ -4,20 +4,68 @@ open NUnit.Framework
 open FsCheck
 open FsCheck.NUnit
 open Library.TreeDesign
+open Lines
+
+
+let rec checkParentCentered (Node((_, pos), subtrees): Tree<(char * float)>) =
+        match subtrees with
+        | [] -> true
+        | _ -> 
+            let childPos = subtrees |> List.map (fun (Node((_, pos), _)) -> pos)
+            let avgChildPos = childPos |> List.average
+            printfn "avgChildPos%f" avgChildPos
+            printfn "pos%f" pos
+            printfn "pos%f" (avgChildPos - pos)
+            abs(avgChildPos - pos) < 0.000001 && (subtrees |> List.forall checkParentCentered)
+
+[<TestCase>]
+let ``test no subtree`` () = 
+    let testTree = Node ('a', [])
+    printfn "1. %A" testTree 
+    let testTree' = design testTree
+    printfn "2. %A" testTree'
+    let result = testTree' |> checkParentCentered
+    Assert.IsTrue(result)
+
+[<TestCase>]
+let ``test subtree with only one node`` () = 
+    let testTree = Node ('a', [Node ('a', []);])
+    printfn "1. %A" testTree 
+    let testTree' = design testTree
+    printfn "2. %A" testTree'
+    let result = testTree' |> checkParentCentered
+    Assert.IsTrue(result)
+
+[<TestCase>]
+let ``test subtree with two node`` () = 
+    let testTree = Node ('a', [Node ('a', []);Node ('a', [])])
+    printfn "1. %A" testTree 
+    let testTree' = design testTree
+    printfn "2. %A" testTree'
+    let result = testTree' |> checkParentCentered
+    Assert.IsTrue(result)
+
+[<TestCase>]
+let ``test subtree with three node`` () = 
+    let testTree = Node ('a', [Node ('a', []);Node ('a', []);Node ('a', [])])
+    printfn "1. %A" testTree 
+    let testTree' = design testTree
+    printfn "2. %A" testTree'
+    let result = testTree' |> checkParentCentered
+    Assert.IsTrue(result)
+
 
 [<Property>]
-let ``Design generates valid trees`` (t: Tree<int>) =
-    let result = design t
-    // some test here
-    true
-[<Property>]
-let ``Design generates valid char trees`` (t: Tree<char>) =
-    let result = design t
-    // some test here
-    printfn "%A" result
-    true
-[<Property>]
-let ``A parent should be centered above its immediate children`` (t:Tree<'a>) = 
-    true
-    // position
+let ``tt`` (t: Tree<char>) = 
+    let testTree = design t
+    printfn "1. %A" testTree 
+    testTree |> checkParentCentered
     
+[<TestCase>]
+let ``test3`` () = 
+    let testTree = Node ('a', [Node ('a', [Node ('a', []);Node ('a', [])]); Node ('a', [])])
+    printfn "1. %A" testTree 
+    let testTree' = design testTree
+    printfn "2. %A" testTree'
+    let result = testTree' |> checkParentCentered
+    Assert.IsTrue(result)
