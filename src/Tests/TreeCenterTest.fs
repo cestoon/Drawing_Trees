4d76174c29a2997456e11afa982fc9ee56b10968
fengbingkun001 <fengbingkun001@ke.com>
2023-06-07 15:37:54 +0200

    change name more formal

--- src/Tests/TreeCenterTest.fs
+++ src/Tests/TreeCenterTest.fs
@@ -13,56 +13,45 @@ let rec checkParentCentered (Node((_, pos), subtrees): Tree<(char * float)>) =
         | _ -> 
             let childPos = subtrees |> List.map (fun (Node((_, pos), _)) -> pos)
             let avgChildPos = childPos |> List.average
-            printfn "avgChildPos%f" avgChildPos
-            printfn "pos%f" pos
-            printfn "pos%f" (avgChildPos - pos)
             abs(avgChildPos - pos) < 0.000001 && (subtrees |> List.forall checkParentCentered)
 
 [<TestCase>]
 let ``test no subtree`` () = 
     let testTree = Node ('a', [])
-    printfn "1. %A" testTree 
     let testTree' = design testTree
-    printfn "2. %A" testTree'
     let result = testTree' |> checkParentCentered
     Assert.IsTrue(result)
 
 [<TestCase>]
 let ``test subtree with only one node`` () = 
     let testTree = Node ('a', [Node ('a', []);])
-    printfn "1. %A" testTree 
     let testTree' = design testTree
-    printfn "2. %A" testTree'
     let result = testTree' |> checkParentCentered
     Assert.IsTrue(result)
 
 [<TestCase>]
-let ``test subtree with two node`` () = 
+let ``test subtree with even nodes`` () = 
     let testTree = Node ('a', [Node ('a', []);Node ('a', [])])
-    printfn "1. %A" testTree 
     let testTree' = design testTree
-    printfn "2. %A" testTree'
     let result = testTree' |> checkParentCentered
     Assert.IsTrue(result)
 
 [<TestCase>]
-let ``test subtree with three node`` () = 
+let ``test subtree with odd nodes`` () = 
     let testTree = Node ('a', [Node ('a', []);Node ('a', []);Node ('a', [])])
-    printfn "1. %A" testTree 
     let testTree' = design testTree
-    printfn "2. %A" testTree'
     let result = testTree' |> checkParentCentered
     Assert.IsTrue(result)
 
 
 [<Property>]
-let ``tt`` (t: Tree<char>) = 
+let ``A parent should be centered above its immediate children`` (t: Tree<char>) = 
     let testTree = design t
     printfn "1. %A" testTree 
     testTree |> checkParentCentered
-    
+
 [<TestCase>]
-let ``test3`` () = 
+let ``a simple three depths tree`` () = 
     let testTree = Node ('a', [Node ('a', [Node ('a', []);Node ('a', [])]); Node ('a', [])])
     printfn "1. %A" testTree 
     let testTree' = design testTree
