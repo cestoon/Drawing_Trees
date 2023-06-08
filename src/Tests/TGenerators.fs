e29f26ac3be0ea7324786e3e7c7de9a71df08dd9
2015-INSPIRER <43725058+2015-INSPIRER@users.noreply.github.com>
2023-06-08 10:23:43 +0200

    Arbitrary generator of Tree

--- src/Tests/TGenerators.fs
+++ src/Tests/TGenerators.fs
@@ -2,6 +2,7 @@ module Tests.TGenerators
 
 open FsCheck
 open Library.TreeDesign
+open System
 
 // --------------- NormalFloat Generator ---------------
 let normalFloatGenerator =
@@ -11,22 +12,45 @@ let normalFloatGenerator =
 type NormalFloatGenerators =
     static member float() = Arb.fromGen normalFloatGenerator
 
-// // --------------- Tree Generator ---------------
-// // Node((l, x: float, subtrees)
-// let tree<'a> =
-//     let rec tree' s =
-//         match s with
-//         | 0 -> Gen.map (fun (l) -> Node(l, [])) Arb.generate<'a>
-//         | n when n>0 ->
-//             let subtrees = tree' (n/2)  |> Gen.sample 0 5 |> Gen.constant 
-//             Gen.map2 (fun (l) ts -> Node(l, ts)) Arb.generate<'a> subtrees 
-//         | _ -> invalidArg "s" "Only positive args are allowed"
-//     Gen.sized tree'
+let safeIntTreeGenerator() =
+    let rec treeGen (n: int) =
+        match n with
+        | 0 ->
+            let label = Arb.generate<int>
+            //Gen.map Node (Gen.constant (label, []))
+            Gen.map (fun (label) -> Node(label, [])) label
+        | _ ->  
+            let rnd = new Random()
+            let nNext = 
+                rnd.Next(0, n)
+            let label = Gen.constant nNext
+            let subtree = 
+                treeGen nNext
+                |> Gen.sample 0 nNext
+                |> Gen.constant
+            Gen.map2 (fun label subtree -> Node(label,subtree)) label subtree
 
-// type TreeGenerator =
-//     static member Tree() =
-//         {new Arbitrary<Tree<char>>() with
-//             override x.Generator = tree<char>
-//             override x.Shrinker t = Seq.empty }
+    //Gen.sized treeGen
+    treeGen 20
 
-// Arb.register<TreeGenerator>() |> ignore
+let rec subtreeSeq (t:Tree<int>) = 
+    match t with 
+    | Node(label, subtreeList) -> seq subtreeList
+
+type TreeGenerator =
+    static member Tree() =
+        {new Arbitrary<Tree<int>>() with
+           override x.Generator = safeIntTreeGenerator()
+           override x.Shrinker (t: Tree<int>) = subtreeSeq t
+        }
+
+Arb.register<NormalFloatGenerators>() |> ignore
+Arb.register<TreeGenerator>() |> ignore
+
+(*
+let ``Design generates valid trees`` (t: Tree<int>) =
+    let result = design t
+    printfn "%A" result
+    true
+Check.Quick ``Design generates valid trees``
+*)
